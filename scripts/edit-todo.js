48af6654c8e60a3110844ddd32c3001bb4d57741
Safat Mobid <safatmobid@gmail.com>
2022-12-13 14:02:32 -0500

    Exercise set 1 completed

--- scripts/edit-todo.js
+++ scripts/edit-todo.js
@@ -1,8 +1,61 @@
-window.onload = function(){
-    document.getElementById("getUserBtn") = getUserBtn
+window.onload = function () {
+    let getUserBtn = document.getElementById("getUserBtn")
     getUserBtn.onclick = getUserBtnClick;
+
+    let updateBtn = document.getElementById("updateBtn")
+    updateBtn.onclick = updateBtnClick;
+
+    let cancelBtn = document.getElementById("cancelBtn")
+    cancelBtn.onclick = cancelBtnClick;
+
+    document.getElementById("infoContainer").style.display = "none";
 }
 
-function getUserBtnClick(){
-    
+function getUserBtnClick() {
+    document.getElementById("infoContainer").style.display = "block";
+    document.getElementById("inputUserField").style.display = "none";
+
+    let idInput = document.getElementById("userIdInput");
+    fetch("http://jsonplaceholder.typicode.com/todos/"+ idInput.value)
+        .then(result => result.json())
+        .then(data => {
+            document.getElementById("idNumber").innerHTML = idInput.value
+
+            document.getElementById("titleInput").value = data.title
+            if (document.getElementById("titleInput").value != data.title) {
+                document.getElementById("titleInput").value = "title not found"
+            }
+            document.getElementById("completedInput").value = data.completed
+            if (document.getElementById("completedInput").value == "undefined") {
+                document.getElementById("completedInput").value = "Completed not found"
+            }
+        })
+        .catch(err => {
+            messageDiv.innerHTML = "Unexpected error";
+        })
+}
+
+function updateBtnClick() { //I guess it works
+    const updateContainer = document.getElementById("updateContainer");
+    let idInput = document.getElementById("userIdInput");
+    let bodyData = {
+        
+        title: document.getElementById("titleInput").value,
+        completed: document.getElementById("completedInput").value
+    }
+    fetch("http://jsonplaceholder.typicode.com/todos/"+ idInput.value, {
+        method: "PUT",
+        body: bodyData,
+        })
+        .then(response => response.json())
+        .then(json => {
+            updateContainer.innerHTML = "User Updated User:" + json.id
+        });
+ /*        .catch(err => {
+        // If the PUT returns an error, ...
+        });) */
+}
+
+function cancelBtnClick() {
+    window.location.href = 'index.html'
 }
--- scripts/new-todo.js
+++ scripts/new-todo.js
@@ -1,8 +1,28 @@
 window.onload = function () {
-    let getUserBtn = document.getElementById("getUserBtn")
-    getUserBtn.onclick = getUserBtnClick;
+    let addBtn = document.getElementById("addBtn")
+    addBtn.onclick = addBtnClick;
 }
 
-function getUserBtnClick() {
+function addBtnClick() {
+    let newInfo = document.getElementById("infoContainer")
+    let bodyData = {
+        userId:
+            document.getElementById("userIdInput").value,
 
+    }
+
+    fetch("http://jsonplaceholder.typicode.com/todos",
+        {
+            method: "POST",
+            body: JSON.stringify(bodyData),
+        })
+        .then(response => response.json())
+        .then(data => {
+           newInfo.innerHTML = "New User: " + data.id /* + " " + data.title; */
+        });
+/*         .catch (err => {
+            document.getElementById("infoContainer").innerHTML = "Unexpected error "
+        }); */
 }
+
+//WORKS I GUESS 
